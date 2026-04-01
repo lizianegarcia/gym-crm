607d6c78f5b8e9a4919026c324e0dc62ef7a063a
lizianegarcia <lizianegarciaa@gmail.com>
2026-04-01 20:27:57 -0300

    fix: memory front

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ import dashboardRoutes from './modules/dashboard/dashboard.routes';
 const app = express();
 
 app.use(cors({
-  origin: "true",
+  origin: true,
   credentials: true,
 }));
 app.use(express.json());
